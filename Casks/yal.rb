b65f5dc4bbdc0b96c6f94e74f567fbf0f3428992
Charles Ede <charlieede01@gmail.com>
2025-09-07 22:05:01 +0100

    new version

--- Casks/yal.rb
+++ Casks/yal.rb
@@ -1,22 +1,22 @@
 cask "yal" do
-  version "0.1.0"
   arch arm: "aarch64", intel: "x64"
 
-  sha256 arm:   "sha256:fb4af9c317bcd0ee859bbef5400d600e55649d5c19f504be8a3cfe7677b9b556",
-         intel: "sha256:249386be63930011761fa742e0492011f26dd6979bde1d1d31067e706856fb75"
+  version "0.1.1"
+  sha256 arm:   "1352baeb29c90d47447b9de9ccbb283b09d2a61686c018281d50a8799c25bd63",
+         intel: "b2f5e08835635b7550098c4d15c7f311963bfca2fc83e5aafac0c2b61e1ac6b5"
 
   url "https://github.com/klaatu01/yal/releases/download/app-v#{version}/yal_#{version}_#{arch}.dmg",
       verified: "github.com/klaatu01/yal/"
   name "YAL — Yet Another Launcher"
-  desc "Tiny, no-nonsense macOS app launcher"
+  desc "Tiny, no-nonsense app launcher"
   homepage "https://github.com/klaatu01/yal"
-
-  app "YAL.app"
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
-  zap trash: ["~/.config/yal"]
+  app "YAL.app"
+
+  zap trash: "~/.config/yal"
 end
