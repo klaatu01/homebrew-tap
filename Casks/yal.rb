3e42045c65d244d074e861f9c0462635d0b7c9cb
Charles Ede <charlieede01@gmail.com>
2025-09-07 22:05:37 +0100

    revert version

--- Casks/yal.rb
+++ Casks/yal.rb
@@ -1,7 +1,7 @@
 cask "yal" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.1.1"
+  version "0.1.0"
   sha256 arm:   "1352baeb29c90d47447b9de9ccbb283b09d2a61686c018281d50a8799c25bd63",
          intel: "b2f5e08835635b7550098c4d15c7f311963bfca2fc83e5aafac0c2b61e1ac6b5"
 
