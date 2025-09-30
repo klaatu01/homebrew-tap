8697a73d0411b1655e98f03c2975c490360dd303
Charles Ede <charlieede01@gmail.com>
2025-09-30 11:45:29 +0100

    update tap

--- Casks/yal.rb
+++ Casks/yal.rb
@@ -2,7 +2,7 @@ cask "yal" do
   arch arm: "aarch64"
 
   version "0.2.0"
-  sha256 arm:   "e33fe64597b8b515267b8df1644f2cc84c0185addeafb41a9e365f45ef78458e"
+  sha256 arm:   "c44cb6ed967a622bc79dcdb4f1cfb8b1067f925a6a22a21462f4be0deb454489"
 
   url "https://github.com/klaatu01/yal/releases/download/app-v#{version}/yal_#{version}_#{arch}.dmg",
       verified: "github.com/klaatu01/yal/"
